e3b3acd29b93e88f10466fad7d2174bef87f4ef1
Carlos R <carlos.r@operamx.com>
2025-02-24 17:10:20 -0600

    Se creo componente para lista de tickets del aldo del administrador

---
+++ src/app/components/tickets/admin-tickets-list/admin-tickets-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-tickets-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-tickets-list.component.html',
+  styleUrl: './admin-tickets-list.component.scss'
+})
+export class AdminTicketsListComponent {
+
+}
