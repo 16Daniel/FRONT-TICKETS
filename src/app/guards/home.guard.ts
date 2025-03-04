7456c74c439c429059676de59d8c5c45611ca634
Carlos R <carlos.r@operamx.com>
2025-03-04 12:02:27 -0600

    Se creo home guard

---
+++ src/app/guards/home.guard.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    const userData = localStorage.getItem('rwuserdatatk');
+    if (!userData) {
+      this.router.navigate(['/auth/login']);
+      return false;
+    }
+
+    const user = JSON.parse(userData);
+    const userRole = user.idRol;
+    
+    // Definir rutas permitidas por rol
+    const roleRoutes: { [key: string]: string } = {
+      '1': '/main/home-a',
+      '2': '/main/home',
+      '4': '/main/home-s',
+    };
+
+    const allowedRoute = roleRoutes[userRole];
+
+    // Si la ruta solicitada ya es la correcta, permitir acceso
+    if (route.routeConfig?.path && `/main/${route.routeConfig.path}` === allowedRoute) {
+      return true;
+    }
+
+    // Redirigir solo si intenta acceder a una ruta diferente
+    if (allowedRoute) {
+      this.router.navigate([allowedRoute]);
+    } else {
+      this.router.navigate(['/auth/login']); // Si el rol no es válido
+    }
+
+    return false;
+  }
+}
