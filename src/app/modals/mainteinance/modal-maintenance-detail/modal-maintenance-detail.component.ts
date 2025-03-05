572eb8d52da0f080fbb76be90b2e1c2d172083e0
Carlos R <carlos.r@operamx.com>
2025-03-04 22:58:53 -0600

    Se creo modal detalle mantenimiento

---
+++ src/app/modals/mainteinance/modal-maintenance-detail/modal-maintenance-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-maintenance-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-maintenance-detail.component.html',
+  styleUrl: './modal-maintenance-detail.component.scss'
+})
+export class ModalMaintenanceDetailComponent {
+
+}
