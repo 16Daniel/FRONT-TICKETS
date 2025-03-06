f0bc5087a2da2e7cb817f26c7cf470569bb3f635
Carlos R <carlos.r@operamx.com>
2025-03-06 11:41:08 -0600

    Funcionamiento de pagina control de versiones

---
+++ src/app/models/control-version.model.ts
@@ -0,0 +1,6 @@
+export class ControlVersion {
+    id: string | any;
+    version: string = '';
+    descripcion: string = '';
+    fecha: Date = new Date()
+}
--- src/app/pages/version-control/version-control.component.ts
+++ src/app/pages/version-control/version-control.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { EditorModule } from 'primeng/editor';
+import { MessageService } from 'primeng/api';
+
+import { ControlVersion } from '../../models/control-version.model';
+import { VersionControlService } from '../../services/version-control.service';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-version-control',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, EditorModule, CommonModule, ToastModule],
+  providers: [MessageService],
   templateUrl: './version-control.component.html',
-  styleUrl: './version-control.component.scss'
+  styleUrl: './version-control.component.scss',
 })
-export class VersionControlComponent {
+export default class VersionControlComponent {
+  controlVersion: ControlVersion = new ControlVersion();
 
+  constructor(
+    private versionControlService: VersionControlService,
+    private messageService: MessageService
+  ) {}
+
+  async enviar(form: NgForm) {
+    if (form.form.status == 'INVALID') {
+      Object.values(form.controls).forEach((control) => {
+        control.markAsTouched();
+      });
+
+      return;
+    }
+
+    try {
+      await this.versionControlService.create({
+        ...this.controlVersion,
+        descripcion: this.controlVersion.descripcion.trim()
+      });
+      this.showMessage('success', 'Success', 'ENVIADO CORRECTAMENTE');
+      this.controlVersion = new ControlVersion();
+    } catch(error: any) {
+      this.showMessage('error', 'Error', error);
+    }
+  }
+
+  showMessage(sev: string, summ: string, det: string) {
+    this.messageService.add({ severity: sev, summary: summ, detail: det });
+  }
 }
