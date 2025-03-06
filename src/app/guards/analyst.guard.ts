5d4c8b190c53453afc24f3ccb48b9f9fe4f379d9
Carlos R <carlos.r@operamx.com>
2025-03-06 12:20:35 -0600

    proteccion de rutas

---
+++ src/app/guards/analyst.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AnalystGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const userData = localStorage.getItem('rwuserdatatk');
+    if (!userData) {
+      this.router.navigate(['/auth/login']);
+      return false;
+    }
+
+    const user = JSON.parse(userData);
+    const userRole = user.idRol;
+
+    if (userRole === '4') {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+      return false;
+    }
+  }
+}
