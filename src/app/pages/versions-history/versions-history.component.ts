56c75e739e714104efcb08b7c107afdb83cd3660
Carlos R <carlos.r@operamx.com>
2025-03-06 12:07:18 -0600

    Creacion de pagina historial de versiones

---
+++ src/app/pages/versions-history/versions-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-versions-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './versions-history.component.html',
+  styleUrl: './versions-history.component.scss'
+})
+export default class VersionsHistoryComponent {
+
+}
