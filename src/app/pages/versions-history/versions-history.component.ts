6b1ab6bad19d9aca6ae2d85f1a1a452c26b41ab2
Carlos R <carlos.r@operamx.com>
2025-03-07 09:45:42 -0600

    Se agrega funcionalidad de historial de versiones

--- src/app/pages/versions-history/versions-history.component.ts
+++ src/app/pages/versions-history/versions-history.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { VersionControlService } from '../../services/version-control.service';
+import { ControlVersion } from '../../models/control-version.model';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-versions-history',
   standalone: true,
-  imports: [],
+  imports: [TableModule],
   templateUrl: './versions-history.component.html',
-  styleUrl: './versions-history.component.scss'
+  styleUrl: './versions-history.component.scss',
 })
 export default class VersionsHistoryComponent {
+  versionActual: ControlVersion | any;
+  versiones: ControlVersion[] = [];
+  versionSeleccionada: any;
 
+  constructor(
+    private versionControlService: VersionControlService,
+    private cdr: ChangeDetectorRef
+  ) {}
+  
+  async ngOnInit(): Promise<void> {
+    await this.versionControlService.getLastVersion().then((result) => {
+      this.versionActual = { ...result };
+      console.log(this.versionActual);
+    });
+
+    await this.versionControlService.get().subscribe((result) => {
+      this.versiones = result;
+      this.cdr.detectChanges();
+
+      console.log(this.versiones);
+    });
+  }
+
+  onClick() {}
 }
