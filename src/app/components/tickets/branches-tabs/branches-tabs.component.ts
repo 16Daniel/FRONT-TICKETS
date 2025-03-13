28e2c4883b4b90a3e972aba4e5f76f672155ed34
Carlos R <carlos.r@operamx.com>
2025-03-13 16:49:40 -0600

    Creacion de componente tabs

---
+++ src/app/components/tickets/branches-tabs/branches-tabs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branches-tabs',
+  standalone: true,
+  imports: [],
+  templateUrl: './branches-tabs.component.html',
+  styleUrl: './branches-tabs.component.scss'
+})
+export class BranchesTabsComponent {
+
+}
