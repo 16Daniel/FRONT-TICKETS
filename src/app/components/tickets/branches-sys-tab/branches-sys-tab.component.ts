086fd03e3dc48874251344510aea26ca7159b638
Carlos R <carlos.r@operamx.com>
2025-03-13 17:37:52 -0600

    creacion de tab sistemas

---
+++ src/app/components/tickets/branches-sys-tab/branches-sys-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branches-sys-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './branches-sys-tab.component.html',
+  styleUrl: './branches-sys-tab.component.scss'
+})
+export class BranchesSysTabComponent {
+
+}
