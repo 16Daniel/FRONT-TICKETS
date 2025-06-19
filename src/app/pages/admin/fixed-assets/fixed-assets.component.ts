2d37b37707fcfa71984af8f2fe9dd10ea2434d04
Carlos R <carlos.r@operamx.com>
2025-06-19 11:39:41 -0600

    -

---
+++ src/app/pages/admin/fixed-assets/fixed-assets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fixed-assets',
+  standalone: true,
+  imports: [],
+  templateUrl: './fixed-assets.component.html',
+  styleUrl: './fixed-assets.component.scss'
+})
+export default class FixedAssetsComponent {
+
+}
