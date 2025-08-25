c017b1a0f0b2bd5ad8d2c24867580726d8706293
Carlos R <carlos.r@operamx.com>
2025-08-25 17:41:43 -0600

    fix colores calendario

--- src/app/modals/Calendar/modal-colors/modal-colors.component.ts
+++ src/app/modals/Calendar/modal-colors/modal-colors.component.ts
@@ -1,88 +1,94 @@
-import { ChangeDetectorRef, Component, EventEmitter, Input, input, Output, type OnInit } from '@angular/core';
-import { ColorUsuario } from '../../../models/color-usuario';
-import { DocumentsService } from '../../../services/documents.service';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output, type OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
-import { CommonModule } from '@angular/common';
-import { ColorPickerModule } from 'primeng/colorpicker';
-import { FormsModule } from '@angular/forms';
-import { DialogModule } from 'primeng/dialog';
-import { Usuario } from '../../../models/usuario.model';
-import { DropdownModule } from 'primeng/dropdown';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { DropdownModule } from 'primeng/dropdown';
+import { DialogModule } from 'primeng/dialog';
+import Swal from 'sweetalert2';
+
+import { Usuario } from '../../../models/usuario.model';
+import { ColorUsuario } from '../../../models/color-usuario';
+import { DocumentsService } from '../../../services/documents.service';
+import { CalendarColorsService } from '../../../services/calendar-colors.service';
 
 @Component({
   selector: 'app-modal-colors',
   standalone: true,
-  imports: [CommonModule,ToastModule,ColorPickerModule,FormsModule,DialogModule,DropdownModule,ButtonModule,TableModule],
-  providers: [MessageService],  
+  imports: [CommonModule, ToastModule, ColorPickerModule, FormsModule, DialogModule, DropdownModule, ButtonModule, TableModule],
+  providers: [MessageService],
   templateUrl: './modal-colors.component.html',
 })
+
 export class ModalColorsComponent implements OnInit {
-public colores:ColorUsuario[] = []; 
-public coleccion:string = 'colores-usuarios'; 
-public formcolor:string = '';
-public usuarioSeleccionado:Usuario|undefined; 
-@Input() showModalColors:boolean = false;  
-@Input() usuariosHelp:Usuario[] = [];
-@Output() closeEvent = new EventEmitter<boolean>();
-constructor(private documentService:DocumentsService,private cdr: ChangeDetectorRef,private messageService: MessageService,){}
-  ngOnInit(): void
-   {
-     this.documentService.get(this.coleccion).subscribe({
-      next: (data) => {
-          this.colores = data;  
-        this.cdr.detectChanges();
-      },
-      error: (error) => {
-        this.showMessage('error', 'Error', 'Error al procesar la solicitud');
-      },
+  public colores: ColorUsuario[] = [];
+  public coleccion: string = 'colores-usuarios';
+  public formcolor: string = '';
+  public usuarioSeleccionado: Usuario | undefined;
+  usuario: Usuario;
+  @Input() showModalColors: boolean = false;
+  @Input() usuariosHelp: Usuario[] = [];
+  @Output() closeEvent = new EventEmitter<boolean>();
+
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private messageService: MessageService,
+    private calendarColorsService: CalendarColorsService) { this.usuario = JSON.parse(localStorage.getItem('rwuserdatatk')!); }
+
+  ngOnInit(): void {
+    this.calendarColorsService.getByArea(this.usuario.idArea).subscribe(result => {
+      this.colores = result;
+    })
+  }
+
+  showMessage(sev: string, summ: string, det: string) {
+    this.messageService.add({ severity: sev, summary: summ, detail: det });
+  }
+
+  onHide() {
+    this.closeEvent.emit(); // Cerrar modal
+  }
+
+  async agregarColor() {
+    let color: ColorUsuario =
+    {
+      idUsuario: this.usuarioSeleccionado!.id,
+      idArea: this.usuario.idArea,
+      color: this.formcolor
+    }
+
+    await this.calendarColorsService.create(color);
+    Swal.fire({
+      title: "OK",
+      text: "CREADO CORRECTAMENTE",
+      icon: "success",
+      customClass: {
+        container: 'swal-topmost'
+      }
     });
-    }
+  }
 
-    showMessage(sev: string, summ: string, det: string) {
-      this.messageService.add({ severity: sev, summary: summ, detail: det });
-    }
-    onHide() {
-      this.closeEvent.emit(); // Cerrar modal
-    }
+  obtenerNombreUsuario(idUsuario: string): string {
+    let nombre = '';
+    let temp = this.usuariosHelp.filter(x => x.id == idUsuario);
+    if (temp.length > 0) { nombre = temp[0].nombre + ' ' + temp[0].apellidoP; }
+    return nombre
+  }
 
-    async agregarColor()
-    {
-      let color:ColorUsuario = 
-      {
-        idUsuario: this.usuarioSeleccionado!.id,
-        color: this.formcolor
+  async EliminarColor(id: string) {
+
+    await this.calendarColorsService.delete(id);
+    Swal.fire({
+      title: "OK",
+      text: "ELIMINADO CORRECTAMENTE",
+      icon: "success",
+      customClass: {
+        container: 'swal-topmost'
       }
-
-      try {
-        await  this.documentService.create(this.coleccion, color); 
-        this.showMessage('success','Success','Guardado correctamente')
-      } catch (error) {
-        
-      }
-     
-    }
-
-    obtenerNombreUsuario(idUsuario:string):string
-    {
-      let nombre = '';
-      let temp = this.usuariosHelp.filter(x => x.id == idUsuario);
-      if(temp.length>0){nombre = temp[0].nombre + ' ' + temp[0].apellidoP; }
-      return nombre
-    }
-  
-    async EliminarColor(id:string)
-    {
-    
-      try {
-        await  this.documentService.deleteDocument(this.coleccion,id); 
-        this.showMessage('success','Success','Eliminado correctamente')
-      } catch (error) {
-        
-      }
-     
-    }
+    });
+  }
 
 }
