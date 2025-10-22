d3f8fd2bb0aeb45471d1066e6cdbc296d055a07f
Arturo-Mejia-dev <arturo.m@operamx.com>
2025-10-22 09:48:57 -0600

    inventarios

--- src/app/pages/stock/captura-inventario-diario/captura-inventario-diario.component.ts
+++ src/app/pages/stock/captura-inventario-diario/captura-inventario-diario.component.ts
@@ -1,14 +1,42 @@
-import { Component, type OnInit } from '@angular/core';
-
+import { ChangeDetectorRef, Component, type OnInit } from '@angular/core';
+import { InventarioService } from '../../../services/inventario.service';
+import { Inventario } from '../../../models/inventario.model';
+import { Usuario } from '../../../models/usuario.model';
+import Swal from 'sweetalert2';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-captura-inventario-diario',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,TableModule],
   templateUrl: './captura-inventario-diario.component.html',
   styleUrl: './captura-inventario-diario.component.scss',
 })
 export default class CapturaInventarioDiarioComponent implements OnInit {
+public catArticulos:Inventario[] = []; 
+public usuario:Usuario;
+  constructor(private invServ:InventarioService,
+    private cdr: ChangeDetectorRef)
+  {
+    this.usuario = JSON.parse(localStorage.getItem('rwuserdatatk')!);
+  }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {  this.obtenerArticulos(); }
 
+  obtenerArticulos()
+  {
+    Swal.showLoading(); 
+    let idSucursal:number = 1; 
+    this.invServ.obtenerArticulos(idSucursal).subscribe({
+      next: (data) => {
+        this.catArticulos = data;
+        Swal.close();
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.log(error);
+        Swal.close();
+      },
+    });
+  }
 }
