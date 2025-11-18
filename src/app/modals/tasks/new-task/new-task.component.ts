da2079e58fe604ce8e93532cb4bc60892ab31230
Carlos R <carlos.r@operamx.com>
2025-11-18 15:36:12 -0600

    creacion de modal para nuevas tareas

---
+++ src/app/modals/tasks/new-task/new-task.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { DialogModule } from 'primeng/dialog';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [DialogModule, FormsModule, CommonModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.scss'
+})
+export class NewTaskComponent {
+  @Input() esNuevoUsuario: boolean = true;
+  @Input() mostrarModal: boolean = false;
+  @Output() closeEvent = new EventEmitter<boolean>();
+
+  onHide = () => this.closeEvent.emit(false);
+
+  enviar(form: NgForm) {
+
+  }
+}
