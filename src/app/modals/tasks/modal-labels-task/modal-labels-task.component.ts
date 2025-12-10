f73f3ab22f24e4d2b44a608cf1133e91bd2a8648
Carlos R <carlos.r@operamx.com>
2025-12-10 17:44:29 -0600

    -

---
+++ src/app/modals/tasks/modal-labels-task/modal-labels-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-labels-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-labels-task.component.html',
+  styleUrl: './modal-labels-task.component.scss'
+})
+export class ModalLabelsTaskComponent {
+
+}
