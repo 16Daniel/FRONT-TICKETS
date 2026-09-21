47d08beefe69d7d85bbddf375c965de990ef3971
Carlos Rodríguez <carlos.r@operamx.com>
2026-09-21 12:15:30 -0600

    -

--- src/app/tickets/components/admin-tabs/admin-tabs.component.ts
+++ src/app/tickets/components/admin-tabs/admin-tabs.component.ts
@@ -1,17 +1,14 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TabViewModule } from 'primeng/tabview';
-import { DropdownModule } from 'primeng/dropdown';
 import { Subscription } from 'rxjs';
 
 import { Usuario } from '../../../usuarios/interfaces/usuario.model';
-import { Ticket } from '../../interfaces/ticket.model';
 import { Sucursal } from '../../../sucursales/interfaces/sucursal.interface';
-import { AdminSysTabComponent } from '../admin-sys-tab/admin-sys-tab.component';
-import { AdminAudioVideoTabComponent } from '../admin-audio-video-tab/admin-audio-video-tab.component';
-import { AdminMaintenanceTabComponent } from '../admin-maintenance-tab/admin-maintenance-tab.component';
-import { AdminCadenaSuministroTabComponent } from '../admin-cadena-suministro-tab/admin-cadena-suministro-tab.component';
+import { AdminAreaTabComponent } from '../admin-area-tab/admin-area-tab.component';
+import { AreasService } from '../../../areas/services/areas.service';
+import { Area } from '../../../areas/interfaces/area.model';
 
 @Component({
   selector: 'app-admin-tabs',
@@ -20,57 +17,49 @@ import { AdminCadenaSuministroTabComponent } from '../admin-cadena-suministro-ta
     FormsModule,
     TabViewModule,
     CommonModule,
-    AdminSysTabComponent,
-    AdminAudioVideoTabComponent,
-    AdminMaintenanceTabComponent,
-    DropdownModule,
-    AdminCadenaSuministroTabComponent,
+    AdminAreaTabComponent
   ],
   templateUrl: './admin-tabs.component.html',
   styleUrl: './admin-tabs.component.scss',
 })
-
-export class AdminTabsComponent {
+export class AdminTabsComponent implements OnInit, OnDestroy {
   sucursal: Sucursal;
   usuario: Usuario;
-  subscripcionTicket: Subscription | undefined;
-  loading: boolean = false;
-  tickets: Ticket[] = [];
-  public todosLostickets: Ticket[] = [];
-  ticket: Ticket | undefined;
-
+  areasTicket: Area[] = [];
+  areasSub: Subscription | undefined;
+  
   activeIndex: number = 0;
   tabsActivos: Record<string, boolean> = {};
-  private unsubscribe!: () => void;
 
-  constructor(private cdr: ChangeDetectorRef) {
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private areasService: AreasService
+  ) {
     this.usuario = JSON.parse(localStorage.getItem('rwuserdatatk')!);
     this.sucursal = this.usuario.sucursales[0];
+  }
 
-    this.activeIndex = 0;
-    if (this.usuario.idArea == '1')
-      this.tabsActivos['SISTEMAS'] = true;
-    if (this.usuario.idArea == '2')
-      // this.tabsActivos['AUDIO Y VIDEO'] = true;
-      this.tabsActivos['SISTEMAS'] = true;
-    if (this.usuario.idArea == '4')
-      this.tabsActivos['MANTENIMIENTO'] = true;
-    if (this.usuario.idArea == '20')
-      this.tabsActivos['CADENA DE SUMINISTRO'] = true;
+  ngOnInit() {
+    this.areasSub = this.areasService.areas$.subscribe((areas) => {
+      let filteredAreas = areas.filter(a => a.activarTickets === true);
+
+      if (this.usuario.idRol !== '1') {
+        filteredAreas = filteredAreas.filter(a => a.id === this.usuario.idArea);
+      }
+
+      this.areasTicket = filteredAreas;
+      
+      if (this.areasTicket.length > 0) {
+        this.tabsActivos[this.areasTicket[0].nombre] = true;
+      }
+      this.cdr.detectChanges();
+    });
   }
 
   ngOnDestroy() {
-    if (this.subscripcionTicket != undefined) {
-      this.subscripcionTicket.unsubscribe();
+    if (this.areasSub) {
+      this.areasSub.unsubscribe();
     }
-
-    if (this.unsubscribe) {
-      this.unsubscribe();
-    }
-  }
-
-  refrescar() {
-    this.cdr.detectChanges();
   }
 
   onTabChange(event: any) {
--- src/app/tickets/components/branches-tabs/branches-tabs.component.ts
+++ src/app/tickets/components/branches-tabs/branches-tabs.component.ts
@@ -10,10 +10,7 @@ import { Ticket } from '../../interfaces/ticket.model';
 import { TicketsService } from '../../services/tickets.service';
 import { BranchesService } from '../../../sucursales/services/branches.service';
 import { Sucursal } from '../../../sucursales/interfaces/sucursal.interface';
-import { DashboardTasksPageComponent } from '../../../tareas/pages/dashboard-tasks-page/dashboard-tasks-page';
-import { EisenhowerMatrixPageComponent } from '../../../tareas/pages/eisenhower-matrix-page/eisenhower-matrix-page';
 import { BranchesOilTabComponent } from '../../../aceites/components/branches-oil-tab/branches-oil-tab.component';
-import { ComensalesPage } from "../../../comensales/pages/comensales-page/comensales-page";
 import { BranchesSysTabComponent } from '../branches-sys-tab/branches-sys-tab.component';
 import { BranchesAudioVideoTabComponent } from '../branches-audio-video-tab/branches-audio-video-tab.component';
 import { BranchesMaintenanceTabComponent } from '../branches-maintenance-tab/branches-maintenance-tab.component';
@@ -31,9 +28,6 @@ import { SucursalCadenaSuministrosTabComponent } from "../sucursal-cadena-sumini
     MultiSelectModule,
     BranchesMaintenanceTabComponent,
     BranchesOilTabComponent,
-    EisenhowerMatrixPageComponent,
-    DashboardTasksPageComponent,
-    ComensalesPage,
     SucursalCadenaSuministrosTabComponent
   ],
   templateUrl: './branches-tabs.component.html',
